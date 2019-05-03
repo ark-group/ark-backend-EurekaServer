420a8da849c3777ba3a4ca6dfc7a12f674198c2c
krrathore1990 <karanr15.rathore@gmail.com>
2019-05-04 01:04:27 +0530

    first commit

---
+++ src/main/java/com/ark/arkbackendEurekaServer/ArkBackendEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ark.arkbackendEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendEurekaServerApplication.class, args);
+	}
+
+}
